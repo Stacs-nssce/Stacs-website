97fcaf0c00a73696d309f30d67ab29add7ba1e82
Nayanjith <nayanjithajith@gmail.com>
2023-12-21 23:06:26 +0530

    Update:Faculties

--- src/pages/faculties/faculties.js
+++ src/pages/faculties/faculties.js
@@ -1,5 +1,59 @@
 import "./faculties.css";
 
 export default function Faculties() {
-    return <div>Faculties</div>;
+    return (
+        <div className="container">
+            <div className="faculties">
+                <div className="faculty-title-container">
+                    <span className="faculty-title">FACULTIES</span>
+                    <div className="side-boreder"></div>
+                </div>
+                <div className="faculty-mem">
+                    <div className="container-y">
+                        <div className="mem-img"></div>
+                        <span className="mem-name">NAME</span>
+                        <span className="mem-position">PROFESSOR</span>
+                    </div>
+                    <div className="container-b">
+                        <div className="mem-img"></div>
+                        <span className="mem-name">NAME</span>
+                        <span className="mem-position">PROFESSOR</span>
+                    </div>
+                    <div className="container-y">
+                        <div className="mem-img"></div>
+                        <span className="mem-name">NAME</span>
+                        <span className="mem-position">PROFESSOR</span>
+                    </div>
+                    <div className="container-b">
+                        <div className="mem-img"></div>
+                        <span className="mem-name">NAME</span>
+                        <span className="mem-position">PROFESSOR</span>
+                    </div>
+                    <div className="container-b">
+                        <div className="mem-img"></div>
+                        <span className="mem-name">NAME</span>
+                        <span className="mem-position">PROFESSOR</span>
+                    </div>
+                    <div className="container-y">
+                        <div className="mem-img"></div>
+                        <span className="mem-name">NAME</span>
+                        <span className="mem-position">PROFESSOR</span>
+                    </div>
+                    <div className="container-b">
+                        <div className="mem-img"></div>
+                        <span className="mem-name">NAME</span>
+                        <span className="mem-position">PROFESSOR</span>
+                    </div>
+                    <div className="container-y">
+                        <div className="mem-img"></div>
+                        <span className="mem-name">NAME</span>
+                        <span className="mem-position">PROFESSOR</span>
+                    </div>
+                </div>
+            </div>
+            <div className="bottom-bar">
+                <img src="./img/image-21.svg" />
+            </div>
+        </div>
+    );
 }
--- src/pages/newsletter/newsletter.js
+++ src/pages/newsletter/newsletter.js
@@ -1,5 +1,18 @@
 import "./newsletter.css";
 
 export default function Newsletter() {
-    return <div>newsletter</div>;
+    return (
+        <div className="container">
+            <div className="newsletter">
+                <div className="newsletter-title-container">
+                    <span className="newsletter-title">NEWSLETTER</span>
+                    <div className="side-boreder"></div>
+                </div>
+                <div className="faculty-mem"></div>
+            </div>
+            <div className="bottom-bar">
+                <img src="./img/image-21.svg" />
+            </div>
+        </div>
+    );
 }
