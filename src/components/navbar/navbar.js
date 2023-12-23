dcce58a2af43f6476febddba22c7772fdbfbc1a8
mohamed arish <arish1248@gmail.com>
2023-12-23 12:54:14 +0530

    Fixed the reactivity of the nav menu and the homepage

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -16,41 +16,29 @@ export default function Navbar() {
                     <img src="./img/logo.svg" alt="stacs logo" />
                 </Link>
                 <ul className="nav-links">
-                    <NavLink to="/Faculties" onClick={toggleMobileMenu}>
-                        Faculties
-                    </NavLink>
-                    <NavLink to="/Activities" onClick={toggleMobileMenu}>
-                        Activities
-                    </NavLink>
-                    <NavLink to="/Awards" onClick={toggleMobileMenu}>
-                        Awards
-                    </NavLink>
-                    <NavLink to="/Newsletter" onClick={toggleMobileMenu}>
-                        Newsletter
-                    </NavLink>
-                    <NavLink to="/Gallery" onClick={toggleMobileMenu}>
-                        Gallery
-                    </NavLink>
-                    <NavLink to="/Blog" onClick={toggleMobileMenu}>
-                        Blog
-                    </NavLink>
-                    <NavLink to="/Commitee" onClick={toggleMobileMenu}>
-                        Commitee
-                    </NavLink>
+                    <NavLink to="/Activities">Activities</NavLink>
+                    <NavLink to="/Awards">Awards</NavLink>
+                    <NavLink to="/Faculties">Faculty</NavLink>
+                    <NavLink to="/Newsletter">Newsletter</NavLink>
+                    <NavLink to="/Gallery">Gallery</NavLink>
+                    <NavLink to="/Blog">Blog</NavLink>
+                    <NavLink to="/Commitee">Commitee</NavLink>
                 </ul>
 
-                <div className="Hamburg" />
+                <div className="Hamburg" onClick={toggleMobileMenu}>
+                    E
+                </div>
             </div>
             {mobileMenuOpen ? (
                 <ul className="m-nav-links">
-                    <NavLink to="/Faculties" onClick={toggleMobileMenu}>
-                        Faculties
-                    </NavLink>
                     <NavLink to="/Activities" onClick={toggleMobileMenu}>
                         Activities
                     </NavLink>
                     <NavLink to="/Awards" onClick={toggleMobileMenu}>
                         Awards
+                    </NavLink>
+                    <NavLink to="/Faculties" onClick={toggleMobileMenu}>
+                        Faculty
                     </NavLink>
                     <NavLink to="/Newsletter" onClick={toggleMobileMenu}>
                         Newsletter
