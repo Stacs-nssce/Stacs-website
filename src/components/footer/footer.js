c63d8e3a668961d76213845976478c9871ad6e81
Nayanjith <nayanjithajith@gmail.com>
2023-12-25 19:29:54 +0530

    Updated new

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,12 +1,12 @@
 import "./footer.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
     return (
         <div className="footer">
             <div>
                 <img src="./img/image 8.png" />
-                <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+                {/* <FontAwesomeIcon icon="fa-brands fa-linkedin" /> */}
             </div>
             <div></div>
             <div></div>
