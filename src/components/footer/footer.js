e4ed6c7d297f5af2468c21770216482f7a7bc7c6
Nayanjith <nayanjithajith@gmail.com>
2023-12-25 09:49:22 +0530

    Update: Footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,16 @@
+import "./footer.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function Footer() {
+    return (
+        <div className="footer">
+            <div>
+                <img src="./img/image 8.png" />
+                <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+            </div>
+            <div></div>
+            <div></div>
+            <img src="./img/image21.png" />
+        </div>
+    );
+}
--- src/pages/faculties/faculties.js
+++ src/pages/faculties/faculties.js
@@ -2,62 +2,52 @@ import "./faculties.css";
 
 export default function Faculties() {
     return (
-        <div className="container">
-            <div className="faculties">
-                <div className="faculty-title-container">
-                    <span className="faculty-title">FACULTIES</span>
-                    <div className="side-border"></div>
-                </div>
-                <div className="faculty-mem">
-                    <div className="container-y">
-                        <div className="mem-img"></div>
-                        <span className="mem-name">NAME</span>
-                        <span className="mem-position">PROFESSOR</span>
-                    </div>
-                    <div className="container-b">
-                        <div className="mem-img"></div>
-                        <span className="mem-name">NAME</span>
-                        <span className="mem-position">PROFESSOR</span>
-                    </div>
-                    <div className="container-y">
-                        <div className="mem-img"></div>
-                        <span className="mem-name">NAME</span>
-                        <span className="mem-position">PROFESSOR</span>
-                    </div>
-                    <div className="container-b">
-                        <div className="mem-img"></div>
-                        <span className="mem-name">NAME</span>
-                        <span className="mem-position">PROFESSOR</span>
-                    </div>
-                    <div className="container-b">
-                        <div className="mem-img"></div>
-                        <span className="mem-name">NAME</span>
-                        <span className="mem-position">PROFESSOR</span>
-                    </div>
-                    <div className="container-y">
-                        <div className="mem-img"></div>
-                        <span className="mem-name">NAME</span>
-                        <span className="mem-position">PROFESSOR</span>
-                    </div>
-                    <div className="container-b">
-                        <div className="mem-img"></div>
-                        <span className="mem-name">NAME</span>
-                        <span className="mem-position">PROFESSOR</span>
-                    </div>
-                    <div className="container-y">
-                        <div className="mem-img"></div>
-                        <span className="mem-name">NAME</span>
-                        <span className="mem-position">PROFESSOR</span>
-                    </div>
-                </div>
+        <div className="faculties">
+            <div className="faculty-title-container">
+                <span className="faculty-title">FACULTIES</span>
+                <div className="side-border"></div>
             </div>
-            <div className="bottom-bar">
-                <img
-                    className="bottom-bar-bg"
-                    src="./img/bottom-bar.png"
-                    alt="bottom-bar"
-                />
-                <img src="./img/image21.png" alt="image21" />
+            <div className="faculty-mem">
+                <div className="container-y">
+                    <div className="mem-img"></div>
+                    <span className="mem-name">NAME</span>
+                    <span className="mem-position">PROFESSOR</span>
+                </div>
+                <div className="container-b">
+                    <div className="mem-img"></div>
+                    <span className="mem-name">NAME</span>
+                    <span className="mem-position">PROFESSOR</span>
+                </div>
+                <div className="container-y">
+                    <div className="mem-img"></div>
+                    <span className="mem-name">NAME</span>
+                    <span className="mem-position">PROFESSOR</span>
+                </div>
+                <div className="container-b">
+                    <div className="mem-img"></div>
+                    <span className="mem-name">NAME</span>
+                    <span className="mem-position">PROFESSOR</span>
+                </div>
+                <div className="container-b">
+                    <div className="mem-img"></div>
+                    <span className="mem-name">NAME</span>
+                    <span className="mem-position">PROFESSOR</span>
+                </div>
+                <div className="container-y">
+                    <div className="mem-img"></div>
+                    <span className="mem-name">NAME</span>
+                    <span className="mem-position">PROFESSOR</span>
+                </div>
+                <div className="container-b">
+                    <div className="mem-img"></div>
+                    <span className="mem-name">NAME</span>
+                    <span className="mem-position">PROFESSOR</span>
+                </div>
+                <div className="container-y">
+                    <div className="mem-img"></div>
+                    <span className="mem-name">NAME</span>
+                    <span className="mem-position">PROFESSOR</span>
+                </div>
             </div>
         </div>
     );
