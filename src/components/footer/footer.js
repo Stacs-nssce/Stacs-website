7d4f9750e7cde64f49dd1937719438454a96b69f
Nayanjith <nayanjithajith@gmail.com>
2023-12-27 23:37:20 +0530

    Footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,16 +1,30 @@
 import "./footer.css";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Footer() {
     return (
         <div className="footer">
-            <div>
-                <img src="./img/image 8.png" />
-                {/* <FontAwesomeIcon icon="fa-brands fa-linkedin" /> */}
-            </div>
-            <div></div>
-            <div></div>
             <img src="./img/image21.png" />
+            <div className="contact">
+                <div className="images">
+                    <img src="./img/image 8.png" />
+                    <div className="links">
+                        <a>
+                            <img src="./img/linkedIn.png" />
+                        </a>
+                        <a>
+                            <img src="./img/instagram.png" />
+                        </a>
+                        <a>
+                            <img src="./img/youtube.png" />
+                        </a>
+                    </div>
+                </div>
+                <div>
+                    <div></div>
+                    EMAIL: XYZ@GMAIL.COM
+                </div>
+            </div>
+            <div>STACS NSSCE © 2023</div>
         </div>
     );
 }
