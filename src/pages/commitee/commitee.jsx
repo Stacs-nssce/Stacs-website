db1b63298ec45c499e4d5ee5f606c7532c200bcf
mohamed arish <arish1248@gmail.com>
2024-02-21 17:34:26 +0530

    Modified the committe page to dynamically load content instead of a statically designed template

--- src/pages/commitee/commitee.jsx
+++ src/pages/commitee/commitee.jsx
@@ -4,12 +4,60 @@ import "./Commitee.css";
 export default function Committee() {
     // Sample data representing team members
     const teamMembers = [
-        { id: 1, name: "John Doe", role: "Frontend Developer" },
-        { id: 2, name: "John Doe", role: "Frontend Developer" },
-        { id: 3, name: "John Doe", role: "Frontend Developer" },
-        { id: 4, name: "John Doe", role: "Frontend Developer" },
-        { id: 4, name: "John Doe", role: "Frontend Developer" },
-        { id: 4, name: "John Doe", role: "Frontend Developer" },
+        {
+            id: 1,
+            name: "John Doe",
+            role: "FrontEnd Developer",
+            socials: {
+                platform: "link",
+                platform2: "link",
+            },
+        },
+        {
+            id: 2,
+            name: "John Doe",
+            role: "Backend Developer",
+            socials: {
+                platform: "link",
+                platform2: "link",
+            },
+        },
+        {
+            id: 3,
+            name: "John Doe",
+            role: "Full-Stack Developer",
+            socials: {
+                platform: "link",
+                platform2: "link",
+            },
+        },
+        {
+            id: 4,
+            name: "John Doe",
+            role: "Developer",
+            socials: {
+                platform: "link",
+                platform2: "link",
+            },
+        },
+        {
+            id: 5,
+            name: "John Doe",
+            role: "Not a Developer",
+            socials: {
+                platform: "link",
+                platform2: "link",
+            },
+        },
+        {
+            id: 6,
+            name: "John Doe",
+            role: "Maybe a not a still can be a Developer",
+            socials: {
+                platform: "link",
+                platform2: "link",
+            },
+        },
 
         // Add more team members as needed
     ];
@@ -30,8 +78,8 @@ export default function Committee() {
                         <div>
                             <img
                                 className="img"
-                                src={require("./assets/demo-image.png")}
-                                alt=""
+                                src="./assets/demo-image.png"
+                                alt={member.name}
                             />
                         </div>
                         <div className="info">
@@ -39,30 +87,16 @@ export default function Committee() {
                             <p>{member.role}</p>
                         </div>
                         <div className="social">
-                            <a href="#">
-                                <img
-                                    src={require("./assets/external-link.svg")}
-                                    alt=""
-                                />
-                            </a>
-                            <a href="#">
-                                <img
-                                    src={require("./assets/github-logo.svg")}
-                                    alt=""
-                                />
-                            </a>
-                            <a href="#">
-                                <img
-                                    src={require("./assets/behance-logo.svg")}
-                                    alt=""
-                                />
-                            </a>
-                            <a href="#">
-                                <img
-                                    src={require("./assets/dribble-logo.svg")}
-                                    alt=""
-                                />
-                            </a>
+                            {Object.keys(member.socials).map((social) => {
+                                return (
+                                    <a href={member.socials[social]}>
+                                        <img
+                                            src={`./assets/${social}.svg`}
+                                            alt={social}
+                                        />
+                                    </a>
+                                );
+                            })}
                         </div>
                     </div>
                 ))}
